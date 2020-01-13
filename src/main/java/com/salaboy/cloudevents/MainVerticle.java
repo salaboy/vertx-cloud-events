13450cf9fe949b31a9b5cb46cad8b07f4b30c0ed
salaboy <salaboy@gmail.com>
2020-01-13 15:36:12 +0000

    adding go service as target for cloud event

--- src/main/java/com/salaboy/cloudevents/MainVerticle.java
+++ src/main/java/com/salaboy/cloudevents/MainVerticle.java
@@ -30,7 +30,7 @@ public class MainVerticle extends AbstractVerticle {
     }
 
     public void sendCloudEvent(CloudEvent cloudEvent){
-        final HttpClientRequest request = vertx.createHttpClient().post(8080, "localhost", "/");
+        final HttpClientRequest request = vertx.createHttpClient().post(80, "http://cloudevents-go.default.example.com", "/");
 
 // add a client response handler
         request.handler(resp -> {
